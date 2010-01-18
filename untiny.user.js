da1f70bfe8fcc5453653cb2fce2cfb75f9d4cd95
Mark Hepburn <Mark.Hepburn@csiro.au>
2010-01-18 18:05:46 +1100

    Major change in operation; use the link domain to index into the services array, instead of doubley-nested for loops as in the original version.  Should be much much quicker now.

--- untiny.user.js
+++ untiny.user.js
@@ -75,44 +75,36 @@ function convertLinks(){
 
   for (var i = 0; i < links.snapshotLength; i++) {
     var link = links.snapshotItem(i);
-    for (var x in this.services){
-      if (typeof(this.services[x]) == 'string') {
-        var linkStr = link + "";
-        var httpServ = 'http://' + services[x];
-        var httpWwwServ = 'http://www.' + services[x];
-        if ( (linkStr.indexOf(httpServ) == 0 ||
-              linkStr.indexOf(httpWwwServ) == 0)
-                &&
-             ((linkStr != httpServ) &&
-              (linkStr != httpWwwServ) &&
-              (linkStr != (httpServ + '/')) &&
-              (linkStr != (httpWwwServ + '/'))) ) {
-          // Create untiny icon, with link (original to start with;
-          // will be replaced with untiny'd version):
-          var icon_link = document.createElement('a');
-          icon_link.setAttribute('href', linkStr);
-          icon_link.setAttribute('id', 'untiny_link_id_' + i);
-          var icon = document.createElement('img');
-          icon.setAttribute('src','http://untiny.alzaid.ws/extra/untiny.png');
-          icon.setAttribute('style', this.untinyIconStyle);
+    var domainRE = /(http:\/\/)?([^\/]+)/; // we are interested in the domain; group 2
 
-          icon_link.appendChild(icon);
-          link.parentNode.insertBefore(icon_link,link.nextSibling);
+    var match = domainRE.exec(link.href);
+    var domain = match ? match[2] : null;
 
-          (function (old_href, iconElmnt, iconLinkElmnt) {
-             GM_xmlhttpRequest({
-               method:'GET',
-               url: this.api_endpoint + 'extract?url=' + old_href +'&format=text',
-               onload: function(o) {
-                 var new_href = o.responseText;
-                 iconElmnt.setAttribute('title', new_href);
-                 iconLinkElmnt.href = new_href;
-               }
-             });
-           }) (link.href, icon, icon_link);
-          break;
-        }
-      }
+    // if we've managed to extract the domain (shouldn't really fail
+    // all that often), and the domain is listed in the untiny-d
+    // services, create an icon with the original target:
+    if (domain && domain in services) {
+      var icon_link = document.createElement('a');
+      icon_link.setAttribute('href', link);
+      icon_link.setAttribute('id', 'untiny_link_id_' + i);
+      var icon = document.createElement('img');
+      icon.setAttribute('src','http://untiny.alzaid.ws/extra/untiny.png');
+      icon.setAttribute('style', this.untinyIconStyle);
+
+      icon_link.appendChild(icon);
+      link.parentNode.insertBefore(icon_link,link.nextSibling);
+
+      (function (old_href, iconElmnt, iconLinkElmnt) {
+         GM_xmlhttpRequest({
+           method:'GET',
+           url: this.api_endpoint + 'extract?url=' + old_href +'&format=text',
+           onload: function(o) {
+             var new_href = o.responseText;
+             iconElmnt.setAttribute('title', new_href);
+             iconLinkElmnt.href = new_href;
+           }
+         });
+       }) (link.href, icon, icon_link);
     }
   }
 }
